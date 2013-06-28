bf2842b9bcb4f5d2db792d01cfdc391c379438b1
Maarten van Gompel <proycon@anaproy.nl>
2013-06-28 15:13:22 +0200

    added MT scoring

--- evaluation.py
+++ evaluation.py
@@ -8,6 +8,7 @@ import os
 import subprocess
 
 from colibrita.format import Reader
+from colibrita.common import log, runcmd, red, green, blue, yellow, white
 
 def main():
     parser = argparse.ArgumentParser(description="Evaluation")
@@ -18,7 +19,11 @@ def main():
     parser.add_argument('-i',dest='casesensitive',help='Measure translation accuracy without regard for case',action='store_false',default=True)
     args = parser.parse_args()
 
-    evaluate(Reader(args.ref), Reader(args.out), args.matrexdir)
+    totalavgaccuracy, totalwordavgaccuracy, matrexsrcfile, matrextgtfile, matrexoutfile = evaluate(Reader(args.ref), Reader(args.out), args.matrexdir)
+
+    if args.matrexdir:
+        mtscore(args.matrexdir, matrexsrcfile, matrextgtfile, matrexoutfile)
+
 
 def evaluate(ref, out, matrexdir, casesensitive=True):
     ref_it = iter(ref)
@@ -132,11 +137,154 @@ def evaluate(ref, out, matrexdir, casesensitive=True):
 
 
     for t,f in (('src',matrexsrc),('ref',matrextgt),('tst',matrexout)):
-
         f.write("</DOC>\n</" + type + "set>")
         f.close()
 
-    return totalavgaccuracy, totalwordavgaccuracy
+    return totalavgaccuracy, totalwordavgaccuracy, matrexsrcfile, matrextgtfile, matrexoutfile
+
+
+def mtscore(matrexdir, sourcexml, refxml, targetxml, WORKDIR = '.'):
+
+    per = 0
+    wer = 0
+    bleu = 0
+    meteor = 0
+    nist = 0
+    ter = 0
+
+    EXEC_MATREX_WER = matrexdir + '/eval/WER_v01.pl'
+    EXEC_MATREX_PER = matrexdir + '/eval/PER_v01.pl'
+    EXEC_MATREX_BLEU = matrexdir + '/eval/bleu-1.04.pl'
+    EXEC_MATREX_METEOR = matrexdir + '/meteor-0.6/meteor.pl'
+    EXEC_MATREX_MTEVAL = matrexdir + '/mteval-v11b.pl'
+    EXEC_MATREX_TER = matrexdir + '/tercom.jar'
+    EXEC_PERL = 'perl'
+    EXEC_JAVA = 'java'
+
+    errors = False
+    if EXEC_MATREX_BLEU and os.path.exists(EXEC_MATREX_BLEU):
+        if not runcmd(EXEC_PERL + ' ' + EXEC_MATREX_BLEU + " -r " + refxml + ' -t ' + targetxml + ' -s ' + sourcexml + ' -ci > ' + 'bleu.score',  'Computing BLEU score'): errors = True
+        if not errors:
+            try:
+                f = open(WORKDIR + '/bleu.score')
+                for line in f:
+                    if line[0:9] == "BLEUr1n4,":
+                        bleu = float(line[10:].strip())
+                        print("BLEU score: ", bleu, file=sys.stderr)
+                f.close()
+            except:
+                log("Error reading bleu.score",red)
+                errors = True
+    else:
+        log("Skipping BLEU (no script found ["+EXEC_MATREX_BLEU+"])",yellow)
+
+    if EXEC_MATREX_WER and os.path.exists(EXEC_MATREX_WER):
+        if not runcmd(EXEC_PERL + ' ' + EXEC_MATREX_WER + " -r " + refxml + ' -t ' + targetxml + ' -s ' + sourcexml + '  > ' + 'wer.score', 'Computing WER score'): errors = True
+        if not errors:
+            try:
+                f = open(WORKDIR + '/wer.score')
+                for line in f:
+                    if line[0:11] == "WER score =":
+                        wer = float(line[12:20].strip())
+                        log("WER score: " + str(wer), white)
+                f.close()
+            except:
+                log("Error reading wer.score",red)
+                errors = True
+    else:
+        log("Skipping WER (no script found ["+EXEC_MATREX_WER+"]) ",yellow)
+
+    if EXEC_MATREX_PER and os.path.exists(EXEC_MATREX_PER):
+        if not runcmd(EXEC_PERL + ' ' + EXEC_MATREX_PER + " -r " + refxml + ' -t ' + targetxml + ' -s ' + sourcexml + '  > ' + 'per.score',  'Computing PER score'): errors = True
+        if not errors:
+            try:
+                f = open(WORKDIR + '/per.score')
+                for line in f:
+                    if line[0:11] == "PER score =":
+                        per = float(line[12:20].strip())
+                        log("PER score: " + str(per), white)
+                f.close()
+            except:
+                log("Error reading per.score",red)
+                errors = True
+    else:
+        log("Skipping PER (no script found ["+EXEC_MATREX_PER+"])",yellow)
+
+    if EXEC_MATREX_METEOR and os.path.exists(EXEC_MATREX_METEOR):
+        if not runcmd(EXEC_PERL + ' -I ' + os.path.dirname(EXEC_MATREX_METEOR) + ' ' + EXEC_MATREX_METEOR + " -s colibrita -r " + refxml + ' -t ' + targetxml + ' --modules "exact"  > ' + 'meteor.score',  'Computing METEOR score'): errors = True
+        if not errors:
+            try:
+                f = open(WORKDIR + '/meteor.score')
+                for line in f:
+                    if line[0:6] == "Score:":
+                        meteor = float(line[7:].strip())
+                        log("METEOR score: " + str(meteor), white)
+                f.close()
+            except:
+                log("Error reading meteor.score",red)
+                errors = True
+    else:
+        log("Skipping METEOR (no script found ["+EXEC_MATREX_METEOR+"])",yellow)
+
+    if EXEC_MATREX_MTEVAL and os.path.exists(EXEC_MATREX_MTEVAL):
+        if not runcmd(EXEC_PERL + ' ' + EXEC_MATREX_MTEVAL + " -r " + refxml + ' -t ' + targetxml + ' -s ' + sourcexml +  '  > ' + 'mteval.score',  'Computing NIST & BLEU scores'): errors = True
+        if not errors:
+            try:
+                f = open(WORKDIR + '/mteval.score')
+                for line in f:
+                    if line[0:12] == "NIST score =":
+                        nist = float(line[13:21].strip())
+                        log("NIST score: ", nist)
+                    if line[21:33] == "BLEU score =":
+                        try:
+                            bleu2 = float(line[34:40].strip())
+                            if bleu == 0:
+                                bleu = bleu2
+                                log("BLEU score: " + str(bleu), white)
+                            elif abs(bleu - bleu2) > 0.01:
+                                log("blue score from MTEVAL scripts differs too much: " + str(bleu) + " vs " + str(bleu2) +  ", choosing highest score")
+                                if bleu2 > bleu:
+                                    bleu = bleu2
+                            else:
+                                log("BLEU score (not stored): " + str(float(line[34:40].strip())))
+                        except:
+                            raise
+                f.close()
+            except:
+                log("Error reading mteval.score",red)
+                errors = True
+    else:
+        log("Skipping MTEVAL (BLEU & NIST) (no script found)", yellow)
+
+    if EXEC_MATREX_TER and os.path.exists(EXEC_MATREX_TER):
+        if not runcmd(EXEC_JAVA + ' -jar ' + EXEC_MATREX_TER + " -r " + refxml + ' -h ' + targetxml + '  > ' + 'ter.score',  'Computing TER score'): errors = True
+        if not errors:
+            try:
+                f = open(WORKDIR + '/ter.score')
+                for line in f:
+                    if line[0:10] == "Total TER:":
+                        ter = float(line[11:].strip().split(' ')[0])
+                        log("TER score: ", ter,white)
+                f.close()
+            except:
+                log("Error reading ter.score",red)
+    else:
+        log("Skipping TER (no script found)",yellow)
+
+
+    log("SCORE SUMMARY\n===================\n")
+    f = open(WORKDIR + '/summary.score','w')
+    s = "BLEU METEOR NIST TER WER PER"
+    f.write(s+ "\n")
+    log(s)
+    s = str(bleu) + " " + str(meteor) + " " + str(nist)  + " " + str(ter) + " " + str(wer)  + " " + str(per)
+    f.write(s + "\n")
+    log(s)
+    f.close()
+
+
+    return not errors
+
 
 
 
