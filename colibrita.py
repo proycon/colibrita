904da5130e11bafa40dd68e73d117b00f2fa1113
Maarten van Gompel <proycon@anaproy.nl>
2013-08-07 13:40:17 +0200

    test

--- colibrita.py
+++ colibrita.py
@@ -39,7 +39,8 @@ try:
             self.numberRequests += 1
             if b'input' in request.args:
                 request.setHeader(b"content-type", b"application/xml")
-                line = str(request.args[b'input'],'utf-8')
+                print("Server input: ", request.args['input'], file=sys.stderr)
+                line = str(" ".join(request.args['input']),'utf-8')
                 sentencepair = plaintext2sentencepair(line)
                 if self.experts:
                     sentencepair = self.experts.processsentence(sentencepair, self.dttable, self.args.leftcontext, self.args.rightcontext, self.args.keywords, self.args.timbloptions + " +vdb -G0", self.lm, self.args.tmweight, self.args.lmweight)
