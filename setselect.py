97146dbb04f04c0d64389a5cff307ef56f119fef
Maarten van Gompel <proycon@anaproy.nl>
2013-10-28 11:16:17 +0100

    fix

--- setselect.py
+++ setselect.py
@@ -48,6 +48,7 @@ def main():
         sys.exit(2)
     try:
         inputset, outputset, offset,maxwords = sys.argv[1:]
+        maxwords = int(maxwords)
     except:
         maxwords = 99
         try:
