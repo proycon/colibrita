96e599315b294e7eaa4bc4056d8b7da53244b66c
Maarten van Gompel <proycon@anaproy.nl>
2014-07-22 19:35:35 +0200

    debug

--- colibrita.py
+++ colibrita.py
@@ -1110,7 +1110,12 @@ def mosesfullsentence(outputfile, testset, mosesclient=None,experts = None,leftc
                                             scores = tmpscores
 
                                     if scores:
-                                        score = tmweights[0] * math.log(scores[0]) + tmweights[1] * math.log(scores[1]) + tmweights[2] * math.log(classifiedfragment.confidence) + tmweights[3] * math.log(scores[3])
+                                        try:
+                                            score = tmweights[0] * math.log(scores[0]) + tmweights[1] * math.log(scores[1]) + tmweights[2] * math.log(alternative.confidence) + tmweights[3] * math.log(scores[3])
+                                        except:
+                                            print(tmweights,file=sys.stderr)
+                                            print(scores,file=sys.stderr)
+                                            raise Exception
                                         score = math.e ** score
                                     else:
                                         raise Exception("Taret fragment not found in phrasetable, shouldn't happen at this point: source=" + inputfragment_s + ", target=" + targetpattern_s)
