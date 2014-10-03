2615ade24412667c34a5438d732db8f75fcef33f
Maarten van Gompel <proycon@anaproy.nl>
2014-10-04 00:16:28 +0200

    update

--- exp/semeval-classifiercentric.sh
+++ exp/semeval-classifiercentric.sh
@@ -6,6 +6,8 @@ CORPUSNAME="ep7os12"
 BINDIR="/vol/customopt/machine-translation/bin"
 MOSESDIR="/vol/customopt/machine-translation/src/mosesdecoder/"
 EXPDIR="/scratch/proycon/colibrita/semeval/"
+TESTDIR="/home/proycon/semeval2014task5-pub/"
+TRAINCONFIGURATIONS="l1r1 l2r1 l2r2 l3r3"
 
 cd $EXPDIR
 
@@ -16,7 +18,7 @@ for LANGPAIR in $LANGPAIRS; do
     cd $LANGPAIR
     if [ $? -ne 0 ]; then
         echo "No such dir: $LANGPAIR" >&2
-        break
+        continue
     fi
 
     if [ ! -f $CORPUS-train.${L2}.lm ]; then
@@ -29,7 +31,72 @@ for LANGPAIR in $LANGPAIRS; do
     ln -s ../$CORPUS-train.${L2}.txt train.$L2
 
 
-    $MOSESDIR/scripts/training/train-model.perl -external-bin-dir $BINDIR  -root-dir . --corpus train --f $L1 --e $L2 --first-step 1 --last-step 9 -reordering msd-bidirectional-fe --lm 0:3:$EXPDIR/$LANGPAIR/$CORPUS-train.en.lm -mgiza -mgiza-cpus $THREADS -core $THREADS -sort-buffer-size 10G -sort-batch-size 253 -sort-compress gzip -sort-parallel $THREADS
+    if [ ! -f model/moses.ini ]; then
+        $MOSESDIR/scripts/training/train-model.perl -external-bin-dir $BINDIR  -root-dir . --corpus train --f $L1 --e $L2 --first-step 1 --last-step 9 -reordering msd-bidirectional-fe --lm 0:3:$EXPDIR/$LANGPAIR/$CORPUS-train.en.lm -mgiza -mgiza-cpus $THREADS -core $THREADS -sort-buffer-size 10G -sort-batch-size 253 -sort-compress gzip -sort-parallel $THREADS
+    fi
+
+    cd ..
+    
+    if [ ! -f train-$CORPUS/model/moses.ini ]; then
+        echo "Moses failed?" >&2
+        continue
+    fi
+
+
+
+    for CONF in $TRAINCONFIGURATIONS; do
+        L=${CONF:1:1}
+        R=${CONF:3:1}
+        if [ ! -d $CORPUS-$CONF ]; then
+            colibrita --trainfortest $TESTDIR/corpus/$LANGPAIR.gold.tokenised.xml  --source $CORPUS-train.${L1}.txt --target $CORPUS-train.${L2}.txt -M train-$CORPUS/model/phrase-table.gz -l $L -r $R -o $CORPUS-$CONF -p 0.0001 
+        fi
+
+        if [ ! -d "$CORPUS-baseline" ]; then 
+            ln -s $CORPUS-$CONF $CORPUS-baseline
+            colibrita --baseline $TESTDIR/corpus/${LANGPAIR}.gold.tokenised.xml -o $CORPUS-baseline
+        fi
+        if [ ! -d $CORPUS-lmbaseline ]; then 
+            ln -s $CORPUS-$CONF $CORPUS-baseline
+            colibrita --baseline $TESTDIR/corpus/${LANGPAIR}.gold.tokenised.xml -o $CORPUS-lmbaseline --lm $CORPUS-train.${L2}.lm
+        fi
+        if [ ! -d "${CORPUS}-mosesbaseline" ]; then 
+            ln -s $CORPUS-$CONF $CORPUS-mosesbaseline
+            colibrita --test $TESTDIR/corpus/${LANGPAIR}.gold.tokenised.xml -Z -o $CORPUS-mosesbaseline --moseslm $CORPUS-train.${L2}.lm
+        fi
+
+        colibrita --test $TESTDIR/corpus/${LANGPAIR}.gold.tokenised.xml -l $L -r $R -o $CORPUS-$CONF 
+        
+        if [ ! -d $CORPUS-${CONF}-lm ]; then
+            ln -s $CORPUS-$CONF $CORPUS-${CONF}-lm
+            colibrita --test $TESTDIR/corpus/${LANGPAIR}.gold.tokenised.xml -l $L -r $R -o $CORPUS-${CONF}-lm --lm $CORPUS-train.${L2}.lm 
+        fi
+
+        if [ ! -d $CORPUS-${CONF}-moses ]; then
+            ln -s $CORPUS-$CONF $CORPUS-${CONF}moses
+            colibrita --test $TESTDIR/corpus/${LANGPAIR}.gold.tokenised.xml -l $L -r $R -Z -o $CORPUS-${CONF}-moses --lm $CORPUS-train.${L2}.lm 
+        fi
+
+
+        if [ ! -d $CORPUS-${CONF}-mosesweighted ]; then
+            ln -s $CORPUS-$CONF $CORPUS-${CONF}-mosesweighted
+            colibrita --test $TESTDIR/corpus/${LANGPAIR}.gold.tokenised.xml -l $L -r $R -X -o $CORPUS-${CONF}-mosesweighted --lm $CORPUS-train.${L2}.lm 
+        fi
+
+        if [ ! -d $CORPUS-${CONF}-mosescut0.7 ]; then
+            ln -s $CORPUS-$CONF $CORPUS-${CONF}-mosescut0.7
+            colibrita --test $TESTDIR/corpus/${LANGPAIR}.gold.tokenised.xml -l $L -r $R -A 0.7 -o $CORPUS-${CONF}-mosescut0.7 --lm $CORPUS-train.${L2}.lm 
+        fi
+
+        if [ ! -d $CORPUS-${CONF}-mosescut0.8 ]; then
+            ln -s $CORPUS-$CONF $CORPUS-${CONF}-mosescut0.8
+            colibrita --test $TESTDIR/corpus/${LANGPAIR}.gold.tokenised.xml -l $L -r $R -A 0.8 -o $CORPUS-${CONF}-mosescut0.8 --lm $CORPUS-train.${L2}.lm 
+        fi
+
+        if [ ! -d $CORPUS-${CONF}-mosescut0.9 ]; then
+            ln -s $CORPUS-$CONF $CORPUS-${CONF}-mosescut0.9
+            colibrita --test $TESTDIR/corpus/${LANGPAIR}.gold.tokenised.xml -l $L -r $R -A 0.9 -o $CORPUS-${CONF}-mosescut0.9 --lm $CORPUS-train.${L2}.lm 
+        fi
+    done
 
 
 done
