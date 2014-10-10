dd295834dff9a86aafd3d571cdf1cd615a5863a1
Maarten van Gompel <proycon@anaproy.nl>
2014-10-10 15:32:48 +0200

    update

--- exp/results2csv.py
+++ exp/results2csv.py
@@ -6,7 +6,7 @@ import glob
 header = "Configuration,Accuracy,Word Accuracy,Recall,BLEU,METEOR,NIST,TER,WER,PER"
 
 for filename in glob.glob("ep7os12-*.summary.score"):
-    name = os.path.basename('-'.join(filename.split('.')[0].split('-')[1:]))
+    name = os.path.basename('-'.join(filename.split('.')[:-2].split('-')[1:]))
     with open(filename) as f:
         line = f.read() #header
         line = f.read()
