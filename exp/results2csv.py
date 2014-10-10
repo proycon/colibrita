085e52682eef6d474ce242a7669078d2922160d7
Maarten van Gompel <proycon@anaproy.nl>
2014-10-10 20:27:04 +0200

    update

--- exp/results2csv.py
+++ exp/results2csv.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
+from __future__ import print_function, unicode_literals, division, absolute_import
+
 import os
 import glob
+import datetime
 
-header = "Configuration,Accuracy,Word Accuracy,Recall,BLEU,METEOR,NIST,TER,WER,PER"
 
+header = ["System","Accuracy","Word Accuracy","Recall","BLEU","METEOR","NIST","TER","WER","PER"]
+
+
+data = {}
 for filename in glob.glob("ep7os12-*.summary.score"):
     name = '.'.join(os.path.basename(filename).split('.')[:-3])
     name = '-'.join(name.split('-')[1:])
@@ -16,9 +22,34 @@ for filename in glob.glob("ep7os12-*.summary.score"):
                 continue
             fields = [ float(x) for x in line.split(' ') ]
             fields = [ str(round(x,4)) for x in fields ]
-            print(name + ',' + ','.join(fields))
+            data[name] = name + ' & ' + ' & '.join(fields) + r'\\'
 
+print(r"\begin{tabular}{l|rrrrrrrrr}")
+print("%generated at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M") + " in " + os.getcwd() )
+print(r"\hline")
+print(" & ".join(header) + r"\\")
+print(r"\hline")
+if 'baseline' in data: print(data['baseline'])
+if 'lmbaseline' in data: print(data['lmbaseline'])
+print(r"\hline")
+for key in sorted(data):
+    if key.find('baseline') == -1 and key.find('moses') == -1:
+        print(data[key])
+print(r"\hline")
+print(r"\end{tabular}")
 
+print()
 
+print(r"\begin{tabular}{l|rrrrrrrrr}")
+print("%generated at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M") + " in " + os.getcwd() )
+print(r"\hline")
+print(" & ".join(header) + r"\\")
+print(r"\hline")
+if 'mosesbaseline' in data: print(data['mosesbaseline'])
+print(r"\hline")
+for key in sorted(data):
+    if key.find('baseline') == -1 and key.find('moses') != -1:
+        print(data[key])
+print(r"\hline")
 
-
+print(r"\end{tabular}")
