93685cf4598c52d8c33afd51067c2255f34e9052
Maarten van Gompel <proycon@anaproy.nl>
2014-10-10 15:26:16 +0200

    added

---
+++ exp/results2csv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+
+header = "Configuration,Accuracy,Word Accuracy,Recall,BLEU,METEOR,NIST,TER,WER,PER"
+
+for filename in glob.glob("ep7os12-*.summary.score"):
+    name = os.path.basename('-'.join(filename.split('.')[0].split('-')[1:]))
+    with open(filename) as f:
+        line = f.read() #header
+        line = f.read()
+        fields = [round(x,4) for x in line.split(' ') ]
+        print(name + ',' + ','.join(fields))
+
+
+
+
+
