117c0a485ce50536e10c4675ae09ba5332fd5030
Maarten van Gompel <proycon@anaproy.nl>
2014-10-10 15:39:21 +0200

    fix

--- exp/results2csv.py
+++ exp/results2csv.py
@@ -6,12 +6,17 @@ import glob
 header = "Configuration,Accuracy,Word Accuracy,Recall,BLEU,METEOR,NIST,TER,WER,PER"
 
 for filename in glob.glob("ep7os12-*.summary.score"):
-    name = os.path.basename('-'.join(filename.split('.')[:-2].split('-')[1:]))
+    name = '.'.join(os.path.basename(filename).split('.')[:-3])
+    name = '-'.join(name.split('-')[1:])
     with open(filename) as f:
-        line = f.read() #header
-        line = f.read()
-        fields = [ str(round(float(x),4) for x in line.split(' ')) ]
-        print(name + ',' + ','.join(fields))
+        first = True
+        for line in f:
+            if first:
+                first = False
+                continue
+            fields = [ float(x) for x in line.split(' ') ]
+            fields = [ str(round(x,4)) for x in fields ]
+            print(name + ',' + ','.join(fields))
 
 
 
