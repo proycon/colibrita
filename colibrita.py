fb74ca6909ddc7e6063d73d66c9527b7de2203d0
Maarten van Gompel <proycon@anaproy.nl>
2014-10-18 10:57:20 +0200

    pruned old keyword generation code (now moved to colibri-mt)

--- colibrita.py
+++ colibrita.py
@@ -16,8 +16,8 @@ import time
 import socket
 import lxml.etree
 import signal
-from collections import defaultdict
-from urllib.parse import quote_plus, unquote_plus
+#from collections import defaultdict
+#from urllib.parse import quote_plus, unquote_plus
 from copy import copy
 
 from colibrita.format import Writer, Reader, Fragment, Alternative
@@ -128,7 +128,7 @@ class ClassifierExperts:
             self.classifiers[sourcefragment].selectedrightcontext = None
             self.classifiers[sourcefragment].selectedkeywords = None
             kwfile = f.replace('.train','.keywords')
-            if os.path.exists(kwfile):
+            if os.path.exists(kwfile): #adheres to the format used by colibri-mt
                 if sourcefragment in self.classifiers:
                     self.classifiers[sourcefragment].keywords = True
                 self.keywords[sourcefragment] = []
@@ -173,220 +173,10 @@ class ClassifierExperts:
         print("Loaded " + str(len(self.classifiers)) + " classifiers",file=sys.stderr)
 
 
-    def counttranslations(self, reader):
-        tcount = defaultdict( lambda: defaultdict(int) )
-        for sentencepair in reader:
-            for left, sourcefragment, right in sentencepair.inputfragments():
-                targetfragment = sentencepair.reffragmentsdict()[sourcefragment.id]
-                tcount[str(sourcefragment)][str(targetfragment)] += 1
-        return tcount
-
-    def countkeywords(self, reader, keywords, compute_bow_params, bow_absolute_threshold, bow_prob_threshold,bow_filter_threshold):
-        print("Counting words for keyword extraction...", file=sys.stderr)
-        wcount = defaultdict(int)
-        wcount_total = 0
-        tcount = defaultdict( lambda: defaultdict(int) )
-        kwcount = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-        for sentencepair in reader:
-            if int(sentencepair.id) % 1000 == 0:
-                print(" Counting words for keyword extraction @" + str(sentencepair.id), file=sys.stderr)
-            for left, sourcefragment, right in sentencepair.inputfragments():
-                for word in sourcefragment:
-                    wcount[word] += 1
-                    wcount_total += 1
-                for word in itertools.chain(left.split(), right.split()):
-                    wcount[word] += 1
-                    wcount_total += 1
-                    targetfragment = sentencepair.reffragmentsdict()[sourcefragment.id]
-                    tcount[str(sourcefragment)][str(targetfragment)] += 1
-                    kwcount[str(sourcefragment)][str(targetfragment)][word] += 1
-
-
-        return wcount, tcount,kwcount, wcount_total
-
-
-    def probability_translation_given_keyword(self, source,target, keyword, kwcount, wcount):
-        source = str(source)
-        target = str(target)
-
-        if not source in kwcount:
-            print("focusword not seen", source, file=sys.stderr)
-            return 0 #focus word has not been counted for
-
-        if not target in kwcount[source]:
-            print("target not seen:", target, file=sys.stderr)
-            return 0 #sense has never been seen for this focus word
-
-        if not keyword in wcount:
-            print("keyword not seen:", keyword, file=sys.stderr)
-            return 0 #keyword has never been seen
-
-        Ns_kloc = 0.0
-        if keyword in kwcount[source][target]:
-            Ns_kloc = float(kwcount[source][target][keyword])
-
-        Nkloc = 0
-        for t in kwcount[source]:
-            if keyword in kwcount[source][t]:
-                Nkloc += kwcount[source][t][keyword]
-
-
-        Nkcorp = wcount[keyword] #/ float(totalcount_sum)
-
-        return (Ns_kloc / Nkloc) * (1/Nkcorp)
-
-
-    def extract_keywords(self, sourcefragment, bow_absolute_threshold, bow_prob_threshold, bow_filter_threshold, kwcount, wcount):
-        print("Extracting keywords for " + sourcefragment + "...", file=sys.stderr)
-
-        sourcefragment = str(sourcefragment)
-
-        if not sourcefragment in kwcount:
-            print("WARNING: No count found!",file=sys.stderr)
-            return [] #focus word has not been counted for
-
-
-        bag = []
-        #select all words that occur at least 3 times for a sense, and have a probability_sense_given_keyword >= 0.001
-        for targetfragment in kwcount[sourcefragment]:
-            for keyword, freq in kwcount[sourcefragment][targetfragment].items():
-                if (wcount[keyword] >= bow_filter_threshold): #filter very rare words (occuring less than 20 times)
-                    if freq>= bow_absolute_threshold:
-                        p = self.probability_translation_given_keyword(sourcefragment, targetfragment, keyword, kwcount, wcount)
-                        if p >= bow_prob_threshold:
-                            bag.append( (keyword, targetfragment, freq, p) )
-
-        if bag:
-            print("\tFound " + str(len(bag)) + " keywords: ", file=sys.stderr)
-            bag = sorted(bag)
-            f = open(self.workdir + '/' + quote_plus(sourcefragment) + '.keywords','w',encoding='utf-8')
-            for keyword, targetfragment, c, p in bag:
-                f.write(keyword + '\t' + str(targetfragment) + '\t' + str(c) + '\t' + str(p) + '\n')
-                print("\t\t", keyword, file=sys.stderr)
-            f.close()
-
-        else:
-            print("\tNo keywords found", file=sys.stderr)
-        return bag
 
 
 
 
-
-    def build(self, reader, leftcontext, rightcontext, dokeywords, compute_bow_params, bow_absolute_threshold, bow_prob_threshold,bow_filter_threshold, timbloptions, limit=None):
-        assert (isinstance(reader, Reader))
-
-
-        if dokeywords:
-            print("Counting keywords", file=sys.stderr)
-            wcount, tcount, kwcount, wcount_total = self.countkeywords(reader, dokeywords, compute_bow_params, bow_absolute_threshold, bow_prob_threshold,bow_filter_threshold)
-        else:
-            print("Gathering initial occurrence count", file=sys.stderr)
-            tcount = self.counttranslations(reader)
-            wcount = {} #not needed
-            kwcount = {} #not needed
-
-        reader.reset()
-
-        #make translation table of direct translation that have only one translation
-        print("Writing direct translation table", file=sys.stderr)
-        dttable = open(self.workdir + '/directtranslation.table','w',encoding='utf-8')
-        for source in tcount:
-            for target in tcount[source]:
-                if len(tcount[str(source)]) == 1:
-                    dttable.write(str(source) + "\t" + str(target) + "\t" + str(tcount[str(source)][str(target)]) + "\n")
-            #gather keywords:
-            if dokeywords:
-                bag = self.extract_keywords(source, bow_absolute_threshold, bow_prob_threshold, bow_filter_threshold, kwcount, wcount)
-                if bag:
-                    self.keywords[source] = bag
-        dttable.close()
-
-
-
-        if not dokeywords and not leftcontext and not rightcontext:
-            print("No classifiers needed, skipping...", file=sys.stderr)
-            return
-
-        index = open(self.workdir + '/index.table','w',encoding='utf-8')
-        #now loop over corpus and build classifiers for those where disambiguation is needed
-        for sentencepair in reader:
-            targetfragments = sentencepair.reffragmentsdict()
-            usedclassifier = False
-            for left, inputfragment, right in sentencepair.inputfragments():
-                if not str(inputfragment) in tcount:
-                    print("WARNING: Inputfragment " + str(inputfragment) + " not found in count! Skipping!!", file=sys.stderr)
-                    continue
-                if limit and not str(inputfragment) in limit:
-                    print("NOTICE: Inputfragment " + str(inputfragment) + " not found in testset, skipping...", file=sys.stderr)
-                    continue
-                left = tuple(left.split())
-                right = tuple(right.split())
-                if len(tcount[str(inputfragment)]) > 1:
-                    usedclassifier = True
-                    #extract local context
-                    features = []
-                    f_left = []
-                    if leftcontext:
-                        f_left = list(left[-leftcontext:])
-                        if len(f_left) < leftcontext:
-                            f_left = list(["<s>"] * (leftcontext - len(f_left))) + f_left
-                    features += f_left
-
-                    f_right = []
-                    if rightcontext:
-                        f_right = list(right[:rightcontext])
-                        if len(f_right) < rightcontext:
-                            f_right = f_right + list(["</s>"] * (rightcontext - len(f_right)))
-                    features += f_right
-
-                    targetfragment = targetfragments[inputfragment.id]
-
-                    #extract global context
-                    if dokeywords and str(inputfragment) in self.keywords:
-                        bag = {}
-                        for keyword, target, freq,p in self.keywords[str(inputfragment)]:
-                            bag[keyword] = 0
-                            if len(bag) == MAXKEYWORDS:
-                                break
-
-                        for word in itertools.chain(left, right):
-                            if word in bag:
-                                bag[keyword] = 1
-
-                        #add to features
-                        for keyword in sorted(bag.keys()):
-                            features.append(keyword+"="+str(bag[keyword]))
-
-                    if not str(inputfragment) in self.classifiers:
-                        #Build classifier
-                        cid = quote_plus(str(inputfragment))
-                        cidfile = self.workdir + '/' +  cid
-                        print("\tInitiating classifier for " + str(inputfragment) + " -- " + cidfile + '.train',file=sys.stderr )
-                        self.classifiers[str(inputfragment)] = timbl.TimblClassifier(cidfile, timbloptions)
-                        index.write(cid + "\t" + str(inputfragment) + "\t" + str(sum(tcount[str(inputfragment)].values())) + "\t")
-                        for target, c in tcount[str(inputfragment)].items():
-                            index.write(str(target) + "\t" + str(c))
-                        index.write("\n")
-                        configid = 'l' + str(leftcontext) + 'r' + str(rightcontext)
-                        if dokeywords: configid += 'k'
-                        f = open(self.classifiers[str(inputfragment)].fileprefix + '.conf', 'w',encoding='utf-8')
-                        f.write("config=" + configid+"\n")
-                        f.close()
-
-                    if not features:
-                        print("WARNING: No features extracted for " + str(inputfragment) + ", skipping classifier!", file=sys.stderr)
-                    else:
-                        print("\tAppending training instance: " +str(inputfragment) + " -> " + str(targetfragment),file=sys.stderr )
-                        self.classifiers[str(inputfragment)].append( features, str(targetfragment) )
-
-            if usedclassifier:
-                print("Built classifier(s) for @" + str(sentencepair.id), file=sys.stderr)
-            elif int(sentencepair.id) % 1000 == 0:
-                print("Skipped @" + str(sentencepair.id), file=sys.stderr)
-        index.close()
-        for i, classifier in enumerate(self.classifiers):
-            self.classifiers[classifier].flush()
 
 
 
@@ -1413,46 +1203,8 @@ def main():
                     print("Read weight reordering = ", repr(args.mosesreorderingweight),file=sys.stderr)
 
 
-    if args.trainfromset:
 
-
-        ####################### TRAIN FROM XML SET (old method) #################################3
-        #TODO!! ADAPT TO NEW COLIBRI-MT STYLE!
-
-        if args.lm:
-            print("WARNING: Language model specified during training, will be ignored", file=sys.stderr)
-
-        print("Parameters: ", repr(args), file=sys.stderr)
-
-        experts = ClassifierExperts(args.output)
-        if args.trainfortest:
-            testset = Reader(args.trainfortest)
-            limit = getlimit(testset)
-        else:
-            limit = None
-
-        data = Reader(args.trainfromset)
-        if not os.path.isdir(args.output):
-            os.mkdir(args.output)
-        if not os.path.exists(args.output + '/phrasetable.colibri.alignmodel-keys'):
-            print("Creating alignment model from Moses phrasetable, unconstrained",file=sys.stderr)
-            r = os.system("colibri-mosesphrasetable2alignmodel -i " + args.phrasetable + " -o " + args.output + "/colibri.alignmodel -j " + str(args.joinedprobabilitythreshold) + " -d " + str(args.divergencefrombestthreshold) + " -S " + args.sourceclassfile + " -T " + args.targetclassfile)
-            if r != 0:
-                print("Failed",file=sys.stderr)
-                sys.exit(2)
-
-            print("Building classifiers", file=sys.stderr)
-            experts.build(data, args.leftcontext, args.rightcontext, args.keywords, args.compute_bow_params, args.bow_absolute_threshold, args.bow_prob_threshold, args.bow_filter_threshold, timbloptions, limit)
-        else:
-            print("Classifiers already built", file=sys.stderr)
-            experts.load(timbloptions, args.leftcontext, args.rightcontext, args.keywords, limit, args.autoconf)
-        if args.autoconf:
-            experts.autoconf(args.folds, args.leftcontext, args.rightcontext, args.keywords, timbloptions, limit)
-        experts.train(args.leftcontext, args.rightcontext, args.keywords, limit)
-
-
-
-    elif args.train or args.trainfortest:
+    if args.train or args.trainfortest:
         ####################### TRAIN FROM SCRATCH #################################3
         if not os.path.isdir(args.output):
             os.mkdir(args.output)
